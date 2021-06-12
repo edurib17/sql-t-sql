7319b9ba714095730e3bc83c86ca83e0fffaa6fb
Eduardo Emanuel Carvalho Ribeiro <70245046+edurib17@users.noreply.github.com>
2021-06-12 12:10:04 -0300

    Update Atividade03.sql

--- Atividade03.sql
+++ Atividade03.sql
@@ -1,3 +1,6 @@
+/*para testar a função, criar uma FUNÇÃO ANÔNIMA que usará um CURSOR para varrer a tabela vendedo (vendors) para pegar os IDs. Mostrar o resultado. Por exemplo: NOTICE: Vendedor: 
+10002 -> 1*/
+
 DECLARE @vend_id INT;
 DECLARE  cursor1 CURSOR FOR SELECT vend_id FROM vendors;
 OPEN cursor1;
